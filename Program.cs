997369b831868cf8b29f56134fc73859d09af3f9
bloknet <barinov_tosha@inbox.ru>
2020-08-26 23:18:51 +0300

    wow its work

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@ namespace CWTest
 {
     class Program
     {
+        public static int StringToNumber(string str){
+            string _str = str;
+            return  Convert.ToInt32(_str);
+          
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            StringToNumber("1234");
         }
     }
 }
